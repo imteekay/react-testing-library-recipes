b74530c4021681f6aabb2f88b30262be0608d4ba
TK <leandrotk100@gmail.com>
2020-08-30 17:02:28 -0300

    Add getAllBy

--- queries/src/App.js
+++ queries/src/App.js
@@ -3,6 +3,13 @@ import React from 'react';
 const App = () => (
   <>
     <h1 data-testid="title">Queries</h1>
+
+    <ul>
+      <li data-testid="list-item">Item 1</li>
+      <li data-testid="list-item">Item 2</li>
+      <li data-testid="list-item">Item 3</li>
+      <li data-testid="list-item">Item 4</li>
+    </ul>
   </>
 );
 
--- queries/src/App.test.js
+++ queries/src/App.test.js
@@ -5,15 +5,41 @@ import App from './App';
 describe('getBy', () => {
   it('get the title', () => {
     render(<App />);
-    const linkElement = screen.getByTestId('title');
-    expect(linkElement).toBeInTheDocument();
+
+    const title = screen.getByTestId(/title/i);
+
+    expect(title).toBeInTheDocument();
   });
 
   it('verify no element', () => {
     render(<App />);
 
     try {
-      screen.getByTestId('subtitle');
+      screen.getByTestId(/subtitle/i);
+    } catch (e) {
+      console.log('Did not find nonexistent element');
+    }
+  });
+});
+
+describe('getAllBy', () => {
+  it('get the items', () => {
+    render(<App />);
+
+    const items = screen.getAllByTestId(/list-item/i);
+
+    expect(items[0].textContent).toEqual('Item 1');
+    expect(items[1].textContent).toEqual('Item 2');
+    expect(items[2].textContent).toEqual('Item 3');
+    expect(items[3].textContent).toEqual('Item 4');
+    expect(items.length).toEqual(4);
+  });
+
+  it('verify no other item', () => {
+    render(<App />);
+
+    try {
+      screen.getAllByTestId(/other-item/i);
     } catch (e) {
       console.log('Did not find nonexistent element');
     }
